8e1691732fea84d32ca6f7d3d411885d2bc9a7bf
Karl Voelker <ktvoelker@gmail.com>
2014-02-22 23:00:15 -0800

    Made state monad type for evaluator

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,13 +1,34 @@
 
 module Evaluator where
 
--- import qualified Data.Graph as G
 import H.Common
 
 import Monad
 import Resource
 import Syntax
 
+data EnvElem =
+    EnvType [(Text, Maybe (Expr Unique))]
+  | EnvValue (Maybe (Expr Unique))
+  deriving (Show)
+
+type Env = [(Unique, EnvElem)]
+
+type EvalM = StateT Env M
+
+makeEnv :: Namespace Unique -> Env
+makeEnv = (>>= f)
+  where
+    f (u, (_, d)) = case d of
+      DNamespace ds -> makeEnv ds
+      DType fs -> return (u, EnvType $ map g fs)
+      DVal (BoundExpr _ expr) -> return (u, EnvValue expr)
+      DPrim _ -> mempty
+    g (u, (_, BoundExpr _ expr)) = (uniqueSourceName u, expr)
+
 evalPhase :: Namespace Unique -> M [Resource]
-evalPhase = todo
+evalPhase ns = evalStateT (evaluate ns) $ makeEnv ns
+
+evaluate :: Namespace Unique -> EvalM [Resource]
+evaluate = todo
 
