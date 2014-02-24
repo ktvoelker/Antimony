8bfe7c0f007a77ca88ae50263d41dc970a89d7c8
Karl Voelker <ktvoelker@gmail.com>
2014-02-23 21:57:06 -0800

    Wrote top level of evaluate

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -30,5 +30,23 @@ evalPhase :: Namespace Unique -> M [Resource]
 evalPhase ns = evalStateT (evaluate ns) $ makeEnv ns
 
 evaluate :: Namespace Unique -> EvalM [Resource]
-evaluate = todo
+evaluate ns = concat <$> mapM f ns
+  where
+    f (u, (_, d)) = case d of
+      DNamespace ds -> evaluate ds
+      DType fs -> concat <$> mapM g fs
+      DVal be -> h u be
+      DPrim _ -> return mempty
+    g (u, (_, be)) = h u be
+    h _ (BoundExpr _ Nothing) = return mempty
+    h u (BoundExpr _ (Just expr)) = do
+      expr' <- evalExpr expr
+      modify $ ((u, EnvValue . Just $ expr') :)
+      return $ exprResources expr'
+
+evalExpr :: Expr Unique -> EvalM (Expr Unique)
+evalExpr = todo
+
+exprResources :: Expr Unique -> [Resource]
+exprResources = todo
 
