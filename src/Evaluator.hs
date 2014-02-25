5e9d3a1f9a9e200c38365639a49698ff2c2ec3a2
Karl Voelker <ktvoelker@gmail.com>
2014-02-24 22:22:21 -0800

    More evaluator work

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -44,8 +44,34 @@ evaluate ns = concat <$> mapM f ns
       modify $ ((u, EnvValue . Just $ expr') :)
       return $ exprResources expr'
 
+getValue :: Unique -> EvalM (Expr Unique)
+getValue u = get >>= maybe (impossible "Name not found in getValue") f . lookup u
+  where
+    f (EnvType _) = impossible "Type found in getValue"
+    f (EnvValue Nothing) = impossible "Abstract value found in getValue"
+    f (EnvValue (Just e)) = return e
+
 evalExpr :: Expr Unique -> EvalM (Expr Unique)
-evalExpr = todo
+evalExpr e@(ELit _) = return e
+evalExpr e@(EFun _ _) = return e
+evalExpr (ERef (Qual u fs)) = getValue u >>= flip (foldM lookupField) fs
+evalExpr (EApp fn args) =
+  evalExpr fn >>= \case
+    EFun ps body -> mapM evalExpr args >>= evalExpr . subst body . zip ps
+    EPrim _ -> todo
+    _ -> impossible "Application of a non-function"
+evalExpr (ERec fs) = ERec <$> f (unzip fs)
+  where
+    f (names, values) = zip names <$> mapM evalExpr values
+evalExpr e@(EPrim _) = return e
+
+subst :: Expr Unique -> [(Unique, Expr Unique)] -> Expr Unique
+subst = todo
+
+lookupField :: Expr Unique -> Text -> EvalM (Expr Unique)
+lookupField (ERec fs) f =
+  maybe todo return . lookup f . map (onFst uniqueSourceName) $ fs
+lookupField _ _ = impossible "Field lookup from a non-record"
 
 exprResources :: Expr Unique -> [Resource]
 exprResources = todo
